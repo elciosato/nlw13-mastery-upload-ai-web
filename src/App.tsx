abee1c73913a942850c94f0797619a909a0a0a0b
Elcio Yoshiaki Sato <elciosato@gmail.com>
2023-09-12 10:22:32 +0200

    installation and configuration shadcn-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { Button } from "./components/ui/button";
+
 export function App() {
-  return <h1>hello world</h1>;
+  return (
+    <div>
+      <h1>hello world</h1>
+      <Button>Test</Button>
+    </div>
+  );
 }
